b72ed92e970dcb66ffb51a378403c902ff21f2a6
Map Bui <bmd08a1@gmail.com>
2019-04-03 13:52:50 +0700

    init, add listNearbyDevicesAddress

---
+++ android/src/main/java/com/reactlibrary/RNZqPrinterUtilsModule.java
@@ -0,0 +1,60 @@
+
+package com.reactlibrary;
+
+import android.util.Log;
+
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.Callback;
+
+import com.zebra.sdk.comm.ConnectionException;
+import com.zebra.sdk.printer.discovery.BluetoothDiscoverer;
+import com.zebra.sdk.printer.discovery.DiscoveredPrinter;
+import com.zebra.sdk.printer.discovery.DiscoveryHandler;
+
+import java.util.ArrayList;
+
+public class RNZqPrinterUtilsModule extends ReactContextBaseJavaModule {
+
+  private final ReactApplicationContext reactContext;
+
+  public RNZqPrinterUtilsModule(ReactApplicationContext reactContext) {
+    super(reactContext);
+    this.reactContext = reactContext;
+  }
+
+  @Override
+  public String getName() {
+    return "RNZqPrinterUtils";
+  }
+
+  @ReactMethod
+  public ArrayList<String> listNearbyDevicesAddress() {
+    ArrayList<String> devicesAddress = new ArrayList<String>();
+
+    try {
+      BluetoothDiscoverer.findPrinters(this.reactContext, new DiscoveryHandler() {
+        @Override
+        public void foundPrinter(DiscoveredPrinter discoveredPrinter) {
+          devicesAddress.add(discoveredPrinter.address)
+          Log.i(getName(), "Discovered a printer");
+        }
+
+        @Override
+        public void discoveryFinished() {
+          Log.i(getName(), "Discovery finished");
+        }
+
+        @Override
+        public void discoveryError(String s) {
+          Log.i(getName(), "Discovery error");
+        }
+      });
+    } catch (ConnectionException e) {
+      Log.i(getName(), "Printer connection error");
+    }
+
+    return devicesAddress;
+  }
+}
---
+++ index.js
@@ -0,0 +1,6 @@
+
+import { NativeModules } from 'react-native';
+
+const { RNZqPrinterUtils } = NativeModules;
+
+export default RNZqPrinterUtils;
