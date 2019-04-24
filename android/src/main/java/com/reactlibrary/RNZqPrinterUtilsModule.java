88bfc7d8ec788043b325a7ebfc5119722e411641
Map Bui <bmd08a1@gmail.com>
2019-04-24 16:19:30 +0700

    use lowercase uuid

--- android/src/main/java/com/reactlibrary/RNZqPrinterUtilsModule.java
+++ android/src/main/java/com/reactlibrary/RNZqPrinterUtilsModule.java
@@ -132,7 +132,7 @@ public class RNZqPrinterUtilsModule extends ReactContextBaseJavaModule {
       ParcelUuid[] deviceUuids = device.getUuids();
       for (ParcelUuid uuid : deviceUuids) {
         Log.d(getName(), uuid.toString());
-        if (uuid.toString() == "0001101-0000-1000-8000-00805F9B34F") {
+        if (uuid.toString() == "00001101-0000-1000-8000-00805f9b34fb") {
           promise.resolve(device.getAddress());
         }
       }
